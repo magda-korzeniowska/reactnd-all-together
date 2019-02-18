47316005e249550aca95b28d2f616fa842d2b0d1
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-02-18 18:56:18 +0100

    Add methods handling new input and submitting it

--- src/AddUser.js
+++ src/AddUser.js
@@ -6,49 +6,85 @@ class AddUser extends Component {
     user : {
       firstName: '',
       lastName: '',
-      userName: ''
+      username: ''
     },
     userExists: false
   }
 
   contactExists = (newUsername) => {
     for (let user of this.props.users) {
-      if (user.userName === newUsername) {
+      if (user.username === newUsername) {
         return true;
-      } else {
-        return false;
       }
     }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const userExists = this.contactExists(this.state.user.username)
+
+    if (!userExists) {
+      this.props.onAddUser(this.state.user)
+      this.setState({
+        user : {
+          firstName: '',
+          lastName: '',
+          username: ''
+        }
+      })
+    }
+    this.setState(() => ({
+      userExists,
+    }))
+  }
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+
+    this.setState(currentState => ({
+      ...currentState,
+      user: {
+        ...currentState.user,
+        [name]: value,
+      },
+    }))
   }
 
   render() {
     return (
       <div>
         <h2>Add New User:</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type='text'
             name='firstName'
             placeholder='Enter Your First Name'
             value={this.state.user.firstName}
-            // onChange={}
+            onChange={this.handleInput}
           />
           <input
             type='text'
             name='lastName'
             placeholder='Enter Your Last Name'
             value={this.state.user.lastName}
-            // onChange={}
+            onChange={this.handleInput}
           />
           <input
             type='text'
-            name='userName'
+            name='username'
             placeholder='Enter Your Username'
-            value={this.state.user.userName}
-            // onChange={}
+            value={this.state.user.username}
+            onChange={this.handleInput}
           />
+          <button disabled={this.state.user.firstName === '' || this.state.user.lastName === '' ||
+            this.state.user.username === ''}
+            >Add New User</button>
         </form>
-
+        {this.state.userExists ? (
+          <p className="error">A user already exists!</p>
+        ) : (
+          ''
+        )}
       </div>
     )
   }
