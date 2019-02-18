69747df279207c7906ed7ae24a3ecf12c2e3cad4
magda-korzeniowska <m.korzeniowska@tlen.pl>
2019-02-18 18:08:06 +0100

    Add method checking if username already exists

--- src/AddUser.js
+++ src/AddUser.js
@@ -11,10 +11,43 @@ class AddUser extends Component {
     userExists: false
   }
 
+  contactExists = (newUsername) => {
+    for (let user of this.props.users) {
+      if (user.userName === newUsername) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Add New User</h2>
+        <h2>Add New User:</h2>
+        <form>
+          <input
+            type='text'
+            name='firstName'
+            placeholder='Enter Your First Name'
+            value={this.state.user.firstName}
+            // onChange={}
+          />
+          <input
+            type='text'
+            name='lastName'
+            placeholder='Enter Your Last Name'
+            value={this.state.user.lastName}
+            // onChange={}
+          />
+          <input
+            type='text'
+            name='userName'
+            placeholder='Enter Your Username'
+            value={this.state.user.userName}
+            // onChange={}
+          />
+        </form>
 
       </div>
     )
